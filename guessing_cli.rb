c644d4e55a75a8f027ffa8426ea9c027e4cef897
Reinald Reynoso <reireynoso@gmail.com>
2019-03-20 18:07:48 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -3,6 +3,8 @@ def run_guessing_game
   puts "Guess a number between 1 and 6."
   guess = gets.chomp
   random = rand(2...6)
+  puts guess
+  puts random
   if guess == "exit"
     puts "Goodbye!"
   elsif

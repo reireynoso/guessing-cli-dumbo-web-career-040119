8193ef0631c145c382ac53efb6294efb5746a9da
Reinald Reynoso <reireynoso@gmail.com>
2019-03-20 18:11:43 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -9,6 +9,7 @@ def run_guessing_game
     puts "Goodbye!"
   elsif guess == random
     puts "You guessed the correct number!"
+    puts "The computer guessed #{random}"
   else
     puts "The computer guessed #{random}"
   end
